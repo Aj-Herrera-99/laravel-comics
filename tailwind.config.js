3daf543224f15f1f178ca137deb01c72097816fc
aj-herrera <ajherrera999@gmail.com>
2025-03-28 16:17:48 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./storage/framework/views/*.php",
+        "./app/View/Components/**/*.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
