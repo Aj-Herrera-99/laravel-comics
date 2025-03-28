0d8833e0284dda988c07dc5f668609e94ac9790b
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-03-28 17:23:07 +0100

    update jumbotron section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
+    <section style="background-image: url('/assets/img/jumbotron.jpg')" class="h-100 bg-no-repeat bg-cover">
+    </section>
     @include('partials.subfooter')
 @endsection
