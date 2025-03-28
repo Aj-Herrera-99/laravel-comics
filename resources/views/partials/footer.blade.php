9a6628cce9b52c4605a5a130c4838274ec4faca3
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-03-28 18:28:17 +0100

    update topfooter

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,56 @@
-<footer class="bg-blue-300 p-4">footer</footer>
+<footer style="background-image: url('/assets/img/footer-bg.jpg')" class="bg-blue-300 bg-no-repeat bg-cover">
+    <div class="top-footer flex justify-between relative overflow-hidden py-8">
+        <div class="grid grid-cols-3 w-[30vw] gap-12 [&_li]:capitalize">
+            <div class="space-y-4">
+                <h5 class="uppercase text-white text-2xl font-bold mb-3">dc comics</h5>
+                <ul class="text-neutral-300">
+                    <li><a href="#">characters</a></li>
+                    <li><a href="#">comics</a></li>
+                    <li><a href="#">movies</a></li>
+                    <li><a href="#">tv</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">news</a></li>
+                </ul>
+                <h5 class="uppercase text-white text-2xl font-bold mb-3">shop</h5>
+                <ul class="text-neutral-300">
+                    <li><a href="#">shop DC</a></li>
+                    <li><a href="#">shop DC collection</a></li>
+                </ul>
+            </div>
+            <div>
+                <h5 class="uppercase text-white text-2xl font-bold mb-3">dc</h5>
+                <ul class="text-neutral-300">
+                    <li><a href="#">terms of use</a></li>
+                    <li><a href="#">privacy policy</a></li>
+                    <li><a href="#">ad choices</a></li>
+                    <li><a href="#">advertising</a></li>
+                    <li><a href="#">jobs</a></li>
+                    <li><a href="#">subscriptions</a></li>
+                    <li><a href="#">talent workshops</a></li>
+                    <li><a href="#">certificates</a></li>
+                    <li><a href="#">ratings</a></li>
+                    <li><a href="#">shop help</a></li>
+                    <li><a href="#">contact us</a></li>
+                </ul>
+            </div>
+            <div>
+                <h5 class="uppercase text-white text-2xl font-bold mb-3">sites</h5>
+                <ul class="text-neutral-300">
+                    <li><a href="#">dc</a></li>
+                    <li><a href="#">MAD magazine</a></li>
+                    <li><a href="#">DC kids</a></li>
+                    <li><a href="#">DC universe</a></li>
+                    <li><a href="#">DC power visa</a></li>
+                </ul>
+            </div>
+        </div>
+        <img class="absolute -top-24 right-24" src="/assets/img/dc-logo-bg.png" alt="dc-logo-bg">
+    </div>
+    <div class="bottom-footer bg-[#303030] flex justify-between py-8">
+        {{-- sign-up btn --}}
+        btn
+        {{-- social media links --}}
+        social media
+    </div>
+</footer>
